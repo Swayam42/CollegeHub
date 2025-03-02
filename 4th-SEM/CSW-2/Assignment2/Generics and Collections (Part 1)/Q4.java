8a089af37ac2a7bea0994815c035db3682023459
Swayam <89599729+Swayam42@users.noreply.github.com>
2025-03-02 20:54:04 +0530

    Compare Student

--- 4th-SEM/CSW-2/Assignment2/Generics and Collections (Part 1)/Q4.java
+++ 4th-SEM/CSW-2/Assignment2/Generics and Collections (Part 1)/Q4.java
@@ -1,83 +1,76 @@
+package Assignment_2.Part_1;
 /*Write a program to create a Student class with member variables name, rollNumber, 
 and totalMark. The program should allow the creation of an array of Student objects 
 and implement a linear search to find a specific student in the array. Additionally, the 
 Student class should implement the Comparable interface by overriding the 
-compareTo() method to facilitate comparisons between student objects.
-*/
+compareTo() method to facilitate comparisons between student objects.  */
 
-package Assignment2_1;
+class Student implements Comparable<Student> {
+    private String name;
+    private int rollNumber;
+    private int totalMark;
 
-class Student1 implements Comparable<Student1>{
-	String name;
-	int rollNumber;
-	int totalMark;
-	public Student1(String name, int rollNumber, int totalMark) {
-		
-		this.name = name;
-		this.rollNumber = rollNumber;
-		this.totalMark = totalMark;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getRollNumber() {
-		return rollNumber;
-	}
-	public void setRollNumber(int rollNumber) {
-		this.rollNumber = rollNumber;
-	}
-	public int getTotalMark() {
-		return totalMark;
-	}
-	public void setTotalMark(int totalMark) {
-		this.totalMark = totalMark;
-	}
-	@Override
-	public int compareTo(Student1 s) {
-		if(this.rollNumber==s.rollNumber) {
-			return 1;
-		}else {
+    public Student(String name, int rollNumber, int totalMark) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.totalMark = totalMark;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public int getTotalMark() {
+        return totalMark;
+    }
+
+    @Override
+    public int compareTo(Student1 s) {
+        if(this.getRollNumber()==s.rollNumber){
+			return 0;
+		}else{
 			return -1;
 		}
-	}
-	
-	public String toString() {
-		return "\nName :"+getName()+"\nRoll Number : "+getRollNumber()+"\nTotal Marks : "+getTotalMark();
-	}
-	
-	
-	
+    }
+
+    @Override
+    public String toString() {
+        return "\nName: " + name + "\nRoll Number: " + rollNumber + "\nTotal Marks: " + totalMark;
+    }
 }
+
 public class Q4 {
-	
-	public static void main(String[] args) {
-		
-		Student1 s1=new Student1("Adu",1,99);
-		Student1 s2=new Student1("Kadu",2,78);
-		Student1 s3=new Student1("Chattu",3,56);
-		Student1 s[]={s1,s2,s3};
-		Student1 s4=new Student1("Banar",4,56);
+    public static void main(String[] args) {
+        Student[] s = {
+            new Student("Adu", 1, 99),
+            new Student("Kadu", 2, 78),
+            new Student("Chattu", 3, a56)
+        };
 
-		for(int i=0;i<s.length;i++) {
-			if(s[i].compareTo(s4)==1){
-				System.out.println("Student found : ");
-				System.out.println(s[i].toString());
-				break;
-			}else {
-				System.out.println("Student Not Found");
-				break;
-			}
-		}
-		
+        Student targetStudent = new Student("Banar", 4, 56);
+        
+        boolean found = false;
+        
+        // Linear Search
+        for (Student student : s) {
+            if (student.compareTo(targetStudent) == 0) {
+                System.out.println("Student found:");
+                System.out.println(student);
+                found = true;
+                break;
+            }
+        }
 
-		
-	}
-
-	
+        if (!found) {
+            System.out.println("Student Not Found");
+        }
+    }
 }
+
 /*Output
 Student Not Found
 */
