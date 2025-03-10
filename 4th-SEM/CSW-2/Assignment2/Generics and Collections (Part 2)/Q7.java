7aa18011e22e4c5b672d341154f96818429a0618
Swayam <89599729+Swayam42@users.noreply.github.com>
2025-03-10 11:52:01 +0530

    Address

--- 4th-SEM/CSW-2/Assignment2/Generics and Collections (Part 2)/Q7.java
+++ 4th-SEM/CSW-2/Assignment2/Generics and Collections (Part 2)/Q7.java
@@ -1,31 +1,50 @@
-package Assignment_2.Part_2;
+/*Write a Java program that includes a class Address with member variables plotNo, at, 
+and post. The class should define a parameterized constructor to initialize these 
+attributes. 
+Create a TreeMap, where the key is the name of a person (String), and the value is an 
+Address object. Insert the required key-value pairs into the TreeMap and use an 
+Iterator to display the entries. */
 import java.util.*;
-class Address{
-private int plotNo;
-private String at;
-private String post;
 
-public Address(int plotNo,String at,String post){
-    this.plotNo=plotNo;
-    this.at=at;
-    this.post=post;
-}
 
-public String toString() {
-    return "Plot No: " + plotNo + ", At: " + at + ", Post: " + post;
-}
+class Address {
+    private int plotNo;
+    private String at;
+    private String post;
 
-}
-public class Q7 {
+    public Address(int plotNo,String at,String post) {
+        this.plotNo=plotNo;
+        this.at=at;
+        this.post=post;
+    }
 
-    public static void main(String[] args) {
-        TreeMap<String,Address> tree=new TreeMap<>();
-        tree.put("GODX", new Address(14, "Soubhagya Nagar", "Bhubaneswar"));
-        tree.put("BeastBoy",new Address(43,"CDA","Cuttack"));
-        Iterator<Map.Entry<String,Address>> itr=tree.entrySet().iterator();
-        while(itr.hasNext()){
-            System.out.println(itr.next());
-        }
-
+    @Override
+    public String toString() {
+        return "Plot No: " +plotNo+", At: " +at +", Post: "+post;
     }
 }
+
+public class Q7 {
+    public static void main(String[] args) {
+      
+        TreeMap<String, Address> addressBook = new TreeMap<>();
+
+       
+        addressBook.put("Amit", new Address(101, "MG Road", "Mumbai"));
+        addressBook.put("Priya", new Address(102, "Park Street", "Kolkata"));
+        addressBook.put("Rajesh", new Address(103, "Dwaraka", "Delhi"));
+
+   
+        Iterator<Map.Entry<String, Address>> iterator = addressBook.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Address> entry = iterator.next();
+            System.out.println("Name: " + entry.getKey() + ", Address: " + entry.getValue());
+        }
+    }
+}
+/*
+Output :
+Name: Amit, Address: Plot No: 101, At: MG Road, Post: Mumbai
+Name: Priya, Address: Plot No: 102, At: Park Street, Post: Kolkata
+Name: Rajesh, Address: Plot No: 103, At: Dwaraka, Post: Delhi
+*/
