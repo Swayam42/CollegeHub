bd90a5dc619d8904986203d2f1bf64418c5cfb92
Swayam <89599729+Swayam42@users.noreply.github.com>
2025-03-12 16:06:00 +0530

    AlphaNumeric in String

---
+++ 4th-SEM/CSW-2/Assignment_3/Q1.java
@@ -0,0 +1,32 @@
+package Assignment_3;
+/*You are given a string containing alphanumeric characters, and your task is to design a 
+Java program that extracts and displays the numeric characters from the given string. If 
+no numeric characters are present, the program should display an appropriate message 
+indicating their absence. Additionally, if the input string is null or empty, the program 
+must throw a NullPointerException with a meaningful error message. */
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		String s="Stri76ng";
+		try {
+			boolean num=false;
+			System.out.println("Numeric Characters : ");
+			for(int i=0;i<s.length();i++) {
+				if(Character.isDigit(s.charAt(i))) {
+					num=true;
+					if(num==true) {
+						System.out.print(s.charAt(i)+" ");
+						
+					}
+				}
+			}System.out.println("Numeric char not found");
+		}
+		catch(NullPointerException e) {
+			System.out.println("Null String");
+			System.out.println(e);
+		}
+
+	}
+
+}
