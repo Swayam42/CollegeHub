64a3ed9e3ebe6e442ebda4d1f34e9c4be285e1f5
Swayam <89599729+Swayam42@users.noreply.github.com>
2025-04-09 16:16:24 +0530

    BST(creation,deletion,search)

--- 4th-SEM/CSW-2/ClassWorks/BST/Main.java
+++ 4th-SEM/CSW-2/ClassWorks/BST/Main.java
@@ -1,4 +1,7 @@
 package BST;
+import java.util.*;
+
+//Blueprint of Node for BST :
 class Node{
 	int data;
 	Node left;
@@ -10,38 +13,110 @@ class Node{
 
 class BST{
 	Node root;
+	
+	//Iterative Insertion Method
 	public void insertIterative(int data) {
 		Node newNode=new Node(data);
 		if(root==null) {
-			root=new Node(data);
+			root=newNode;
 			return;
 		}
+		
 		Node curr=root;
 		Node parent=null;
-		while(true) {
+		while(true){
 			parent=curr;
 			if(data<curr.data) {
 				curr=curr.left;
 				if(curr==null) {
-					parent.left=new Node(data);
+					parent.left=newNode;
 					return;
 				}
 			}else {
 				curr=curr.right;
 				if(curr==null) {
-					parent.right=new Node(data);
+					parent.right=newNode;
 					return;
 				}
 			}
 		}
 	}
 	
+	//In-order Traversal Method
 	public void inorder(Node node) {
 		if(node==null) 
 			return;
 		inorder(node.left);
-		System.out.println(node.data);
+		System.out.print(node.data+" ");
 		inorder(node.right);
+	}
+	
+	//Deletion Method
+	public void delete(int data) {
+		root=deleteNode(root,data);
+	}
+	//Helper Method for delete()
+	public Node deleteNode(Node root,int data) {
+		if(root==null) {
+			return root;
+		}
+		if(data<root.data){ //Move to left subtree
+			root.left=deleteNode(root.left,data);
+		}else if(data>root.data){ //Move to right subtree
+			root.right=deleteNode(root.right,data);
+		}else{
+			
+			//CASE 1 :Node having one child
+			if(root.left == null){
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+			//CASE 2 :Node having 2 children  
+			 root.data = minSuccessor(root);
+	         root.right = deleteNode(root.right, root.data);
+	         
+	         //CASE 3 :Node having no child (Leaf Node)
+	         if(root.left==null&&root.right==null) {
+	        	 return null;
+	         }
+		}
+		
+		return root;
+		
+	}
+	
+	//Smallest Node in the right subtree
+	public int minSuccessor(Node root) {
+		root=root.right;
+		int min=root.data;
+		while(root.left!=null) {
+			min=root.left.data;
+			root=root.left;
+		}
+		return min;
+	}
+	
+	//Search Node :
+	
+	public boolean SearchNode(int data) {
+		return search(root,data);
+	}
+	
+	public boolean search(Node root,int data) {
+		if(root==null) {
+			return false;
+		}
+		else if(root.data==data) {
+			return true;
+		}
+		else if(data<root.data) {
+			return search(root.left,data);
+		}else {
+			 return search(root.right,data);
+		}
+	
+		
 	}
 	
 	
@@ -50,14 +125,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		BST tree=new BST();
-		int arr[]= {10,6,4,8,2};
+		int arr[]= {45,10,90,7,12,50};
 		for(int i:arr) {
 			tree.insertIterative(i);
 		}
 		
-		System.out.println("Inorder Traversal : ");
+		System.out.print("Inorder Traversal : ");
 		tree.inorder(tree.root);
+		
+		//Deletion of Node :
+		int delData=45;
+		tree.delete(delData);
+		System.out.print("\nAfter Deleting 45 :");
+		tree.inorder(tree.root);
+		System.out.println();
+		System.out.println(tree.SearchNode(3));
 
 	}
+
+	
 
 }
