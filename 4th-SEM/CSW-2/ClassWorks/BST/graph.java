d6b86956b1a2bcc7051f7f319db75242095045b4
Swayam <89599729+Swayam42@users.noreply.github.com>
2025-04-11 11:08:46 +0530

    Update graph.java

--- 4th-SEM/CSW-2/ClassWorks/BST/graph.java
+++ 4th-SEM/CSW-2/ClassWorks/BST/graph.java
@@ -1,6 +1,6 @@
 package BST;
 
-
+import java.util.*;
 class Vertex{
 	char c;
 	boolean wasVisited;
@@ -11,14 +11,14 @@ class Vertex{
 	
 }
 public class Graph {
-	int i=0;
-	int maxVert=10;
+	int n=0;
+	int maxVert=20;
 	Vertex vertexList[]=new Vertex[maxVert];
 	int adjMat[][]=new int [maxVert][maxVert];
 	
 	public void addVertex(char val) {
-		vertexList[i]=new Vertex(val);
-		i++;
+		vertexList[n]=new Vertex(val);
+		n++;
 	}
 	
 	public void addEdge(int start,int end) {
@@ -26,34 +26,74 @@ public class Graph {
 		adjMat[end][start]=1;
 	}
 	
-	public void display() {
+	public void display(){
 		System.out.print("  ");
-        for (int j = 0; j < i; j++) {
+        for (int j=0;j<n;j++){
             System.out.print(vertexList[j].c + " ");
         }
         System.out.println();
-        for (int row = 0; row < i; row++) {
+        for (int row=0;row<n;row++){
             System.out.print(vertexList[row].c + " ");
-            for (int col = 0; col < i; col++) {
-                System.out.print(adjMat[row][col] + " ");
+            for (int col=0;col<n;col++){
+                System.out.print(adjMat[row][col]+ " ");
             }
             System.out.println();
         }
 	}
 	
 	
+	public void DFS() {
+	    Stack<Integer> stack = new Stack<>();
+	    vertexList[0].wasVisited =true;
+	    System.out.print(vertexList[0].c+" ");
+	    stack.push(0);
+
+	    while (!stack.isEmpty()){
+	        int v=stack.peek();
+	        
+	        int adjVertex = getAdjacentVertex(v);
+
+	        if (adjVertex==-1){
+	            stack.pop();
+	        } else {
+	            vertexList[adjVertex].wasVisited=true;
+	            System.out.print(vertexList[adjVertex].c+" ");
+	            stack.push(adjVertex);
+	        }
+	    }
+	    //ReInitialising the 
+	    for (int i=0;i<n;i++){
+	        vertexList[i].wasVisited = false;
+	    }
+	}
+	
+	//helper method for DFS
+	public int getAdjacentVertex(int v) {
+		for(int i=0;i<n;i++) {
+			if(adjMat[v][i]==1&&vertexList[i].wasVisited==false)
+				return i;
+		}
+		return -1;
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
 		Graph g=new Graph();
-		char ver[]= {'A','B','C','D','E','F'};
+		char ver[]= {'A','B','C','D','E'};
 		for(char i:ver) {
 			g.addVertex(i);
 		}
-		 	    g.addEdge(0, 1); // A-B
-	        g.addEdge(1, 2); // B-C
-	        g.addEdge(2, 3); // C-D
-	        g.addEdge(3, 4); // D-E
-	        g.addEdge(4, 0); // E-A
+		 	g.addEdge(0,1); // A-B
+	        g.addEdge(0,2);
+	        g.addEdge(0,3); 
+	        g.addEdge(1,2);
+	        g.addEdge(2,4);
+	        System.out.println("Adjacency Matrix Representation: ");
 	        g.display();
+	        System.out.print("\nDFS traversal :");
+	        g.DFS();
 
 	}
 
