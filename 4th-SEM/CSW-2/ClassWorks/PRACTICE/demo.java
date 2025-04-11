37c55adaa34119d87d5914c0a5c89b26b4c94816
Swayam <89599729+Swayam42@users.noreply.github.com>
2025-04-11 11:10:21 +0530

    Create demo.java

---
+++ 4th-SEM/CSW-2/ClassWorks/PRACTICE/demo.java
@@ -0,0 +1,32 @@
+
+/*create a class my arithmetic exception by extending the required class and variables 
+ * create another calculator app add main method which reads 2 integers and perform addition ,
+ * multiplication,division and subtraction and print the results
+ * add the required technique which throw and handles my arithmetic exception if 
+ * first no.is smaller than second num ..
+ * also add required technique to handle division /zero  exception
+ * */
+public class Calculator {
+
+	public static void main(String[] args){
+		
+
+	}
+
+}
+/*
+ * 
+ * 
+ * 
+ * create an employee class having name,age and dob ... add the required 
+ * methods.. add a method to generate password takes last name from 
+ * name and year of dob 
+ * 
+ * b. write a method to employee class that prints the details of student 
+ * in the format 
+ * first name :
+ * last name :
+ * dob :
+ * age :*/
+
+ */
